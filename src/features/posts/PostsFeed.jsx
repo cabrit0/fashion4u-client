4fe7a94d66cec5df05734ad4c870ac10af325cd5
cabrit0 <cabrit0o.dev@gmail.com>
2023-02-02 19:00:58 +0000

    modal onClick userProfile

--- src/features/posts/PostsFeed.jsx
+++ src/features/posts/PostsFeed.jsx
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from "react";
 import Avatar from "../../components/Avatar";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { likePost, likeComment } from "../likes/likesSlice";
 import { fetchAllPosts } from "../../features/posts/globalPostsSlice";
 import { createComment } from "../comments/commentsSlice";
 
 import { RiHeart2Fill, RiHeart2Line } from "react-icons/ri";
 import { TfiCommentAlt, TfiComment } from "react-icons/tfi";
-import { comment } from "postcss";
 
 const PostsFeed = ({ posts }) => {
   const dispatch = useDispatch();
   const [showComment, setShowComment] = useState([]);
   const [text, setText] = useState("");
   const [update, setUpdate] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [user, setUser] = useState(null);
+
+/*  LEMBRAR DE FAZER A LOGICA PARA O USER CLICADO E NAO PARA O CURRENT USER  */
+
+  const currentUser = useSelector((state) => state.auth.user.user);
+  const allUsers = useSelector((state) => state.allUsers.allUsers);
+
+  const currentUserFollowing = allUsers.filter((user) =>
+    currentUser.following.includes(user._id)
+  );
+  const currentUserFollowers = allUsers.filter((user) =>
+    currentUser.followers.includes(user._id)
+  );
 
   const toggleComment = (index) => {
     const newShowComment = [...showComment];
@@ -37,9 +50,20 @@ const PostsFeed = ({ posts }) => {
     const body = { postId, text };
     const response = dispatch(createComment(body));
     //console.log(response, body);
-    setText('')
+    setText("");
     setUpdate(true);
-    toggleComment()
+    toggleComment();
+  };
+
+  const handleShowAvatarProfile = (user) => {
+    setUser(user);
+    setShowModal(true);
+    console.log(currentUserFollowing);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setUser(null);
   };
 
   useEffect(() => {
@@ -53,7 +77,9 @@ const PostsFeed = ({ posts }) => {
         <div key={post._id} className="bg-slate-800 my-4 rounded-t-2xl">
           <div className="flex justify-between items-center px-4 py-1">
             <div>
-              <Avatar height="8" width="8" user={post.user} />
+              <button onClick={() => handleShowAvatarProfile(post.user)}>
+                <Avatar height="8" width="8" user={post.user} />
+              </button>
             </div>
             <p>{post.user.name}</p>
           </div>
@@ -100,7 +126,6 @@ const PostsFeed = ({ posts }) => {
                       <button
                         onClick={() => handleLikeComment(comment.comment._id)}
                       >
-                        {/* send postID */}
                         <RiHeart2Line className="text-lux-pink text-sm hover:scale-105 hover:translate-x-0.5 hover:-translate-y-1 duration-500" />
                       </button>
                       <p className="mx-2 text-gray-400">
@@ -135,6 +160,46 @@ const PostsFeed = ({ posts }) => {
           </div>
         </div>
       ))}
+      {showModal && (
+        <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-90 flex items-center justify-center">
+          <div className="bg-slate-800 w-11/12 md:w-1/2 xl:w-1/3 p-6 rounded-lg shadow-3xl">
+            <div className="flex items-center mb-4">
+              <Avatar height="20" width="20" user={user} />
+              <h2 className="ml-4 text-gray-200 text-2xl font-medium">
+                {user.name}
+              </h2>
+            </div>
+            <p className="text-purple-400 mb-2">
+              Followers:{" "}
+              <span className="text-fuchsia-500">
+                {currentUserFollowers.length}
+              </span>
+            </p>
+            <p className="text-purple-400 mb-6">
+              Following:{" "}
+              <span className="text-fuchsia-500">
+                {currentUserFollowing.length}
+              </span>
+            </p>
+            <div className="flex justify-center">
+              <button className="bg-lux-blue text-gray-200 font-bold py-1 px-2 mx-1 rounded-full hover:scale-105 hover:translate-x-1 hover:-translate-y-1 duration-500">
+                See {user.name} profile
+              </button>
+              <button className="bg-fuchsia-600 text-gray-200 font-bold py-1 px-2 mx-1 rounded-full hover:scale-105 hover:translate-x-1 hover:-translate-y-1 duration-500">
+                Follow {user.name}
+              </button>
+            </div>
+            <div className="w-full my-4 flex items-center justify-center">
+              <button
+                className=" bg-red-500 text-gray-200 font-bold py-1 px-4 rounded-full hover:scale-105 hover:translate-x-1 hover:-translate-y-1 duration-500"
+                onClick={() => setShowModal(false)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
