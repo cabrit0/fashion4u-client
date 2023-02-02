dc571ebdda0ca73ec713a0fc50c79d6abf5973a7
cabrit0 <cabrit0o.dev@gmail.com>
2023-02-02 21:41:58 +0000

    AxiosURLToClientOnceAgain

--- src/app/api/axiosInstance.js
+++ src/app/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //http://localhost:8080
 //https://fasion4u-server.onrender.com
-const baseURL = "http://localhost:8080/api/v1/";
+const baseURL = "https://fasion4u-server.onrender.com/api/v1/";
 
 export const axiosInstance = axios.create({
   baseURL,
